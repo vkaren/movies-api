3d0efc4a08059e45e0b44e89830331a10506cca8
Karen <karenperarnau@gmail.com>
2023-05-20 14:48:43 -0430

    Deploy config

--- config/config.js
+++ config/config.js
@@ -3,8 +3,7 @@ require("dotenv").config();
 const config = {
   env: process.env.NODE_ENV || "dev",
   uri:
-    process.env.ELEPHANTSQL_URL ||
-    "postgres://postgres:5432@localhost/postgres",
+    process.env.DATABASE_URL || "postgres://postgres:5432@localhost/postgres",
 };
 
 module.exports = config;
